a89cfd43f28df66460c70886455bfa9eea4a540a
Hanadi92 <hanadi.tamimi@gmail.com>
2017-02-09 17:45:25 +0100

    covering all relations except functions

--- modelRDF/src/modelrdf/ModelRDF.java
+++ modelRDF/src/modelrdf/ModelRDF.java
@@ -12,23 +12,16 @@ import java.io.PrintWriter;
 import java.util.Map.Entry;
 import java.util.Set;
 
-import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.Literal;
-import org.eclipse.rdf4j.model.Model;
-import org.eclipse.rdf4j.model.Statement;
 //import org.eclipse.rdf4j.model.Statement;
-import org.eclipse.rdf4j.model.ValueFactory;
-import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
-import org.eclipse.rdf4j.model.impl.TreeModel;
+//import org.eclipse.rdf4j.repository.RepositoryResult;
+import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.util.ModelBuilder;
-import org.eclipse.rdf4j.model.vocabulary.FOAF;
 import org.eclipse.rdf4j.query.BindingSet;
 import org.eclipse.rdf4j.query.TupleQuery;
 import org.eclipse.rdf4j.query.TupleQueryResult;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.RepositoryException;
-import org.eclipse.rdf4j.repository.RepositoryResult;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.RDFParseException;
@@ -51,14 +44,19 @@ public class ModelRDF {
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
-//        String path = "../models/XML.megal";
-//        MegaModel megaModel = createMegaModel(path);
-//        ModelBuilder RdfModel = createRDFModel();
-//        ModelBuilder theRDFModel = extractRelation(megaModel, RdfModel);
-//        Model modelRDF = theRDFModel.build();
-//        printOutRDFModel(modelRDF);
+        String path = "../models/XML.megal";
+        MegaModel megaModel = createMegaModel(path);
+        ModelBuilder RdfModel = createRDFModel();
+        extractRelationshipInstance(megaModel, RdfModel);
+        extractInstanceOf(megaModel, RdfModel);
+        extractRelationshipDeclaration(megaModel, RdfModel);
+        extractLinkMap(megaModel, RdfModel);
+        extractSubtypes(megaModel, RdfModel);
+        Model modelRDF = RdfModel.build();
+        printOutRDFModel(modelRDF);
         createDatabase();
-    }
+        
+}
 
     private static void createDatabase() throws RDFParseException, RepositoryException, IOException {
     	// Create a new Repository. Here, we choose a database implementation
@@ -71,29 +69,30 @@ public class ModelRDF {
     		// add the RDF data from the input stream directly to our database
     		conn.add(input, "", RDFFormat.TURTLE );
     	    }
-    		// let's check that our data is actually in the database
-    		try (RepositoryResult<Statement> result = conn.getStatements(null, null, null);) {
-    			while (result.hasNext()) {
-    				Statement st = result.next();
+    		// check the data in the database
+//    		try (RepositoryResult<Statement> result = conn.getStatements(null, null, null);) {
+//    			while (result.hasNext()) {
+//    				Statement st = result.next();
 //    				System.out.println("db contains: " + st);
-    			}
-    		}
+//    			}
+//    		}
     		// We do a simple SPARQL SELECT-query that retrieves all resources of
     		// type `ex:Artist`, and their first names.
     		String queryString  = "prefix XML: <https://en.wikipedia.org/wiki/XML/>\n";
     		queryString  += "prefix xsd: <http://www.w3.org/2001/XMLSchema#>\n";
-    		queryString += "SELECT ?x ?y WHERE { ?x XML:aspectOf ?y }";
+    		queryString += "SELECT ?x ?y WHERE { ?x XML:implements ?y }";
     	    TupleQuery query = conn.prepareTupleQuery(queryString);
     	    // A QueryResult is also an AutoCloseable resource, so make sure it gets 
     	    // closed when done.
     	    try (TupleQueryResult result = query.evaluate()) {
+    	    	System.out.println("\nThe Query: " + queryString + "\n");
     		// we just iterate over all solutions in the result...
 	    		while (result.hasNext()) {
 	    		    BindingSet solution = result.next();
 	    		    // ... and print out the value of the variable bindings
 	    		    System.out.println(solution);
-	    		    System.out.println("?s = " + solution.getValue("s"));
-	    		    System.out.println("?n = " + solution.getValue("n"));
+//	    		    System.out.println("?x = " + solution.getValue("x"));
+//	    		    System.out.println("?y = " + solution.getValue("y"));
 	    		}
     	    }
     	}
@@ -107,7 +106,7 @@ public class ModelRDF {
     	try{
     	    PrintWriter writer = new PrintWriter(fileName, "UTF-8");
     	    Rio.write(theRDFModel, writer, RDFFormat.TURTLE);
-    	    Rio.write(theRDFModel, System.out, RDFFormat.TURTLE);
+//    	    Rio.write(theRDFModel, System.out, RDFFormat.TURTLE);
     	    writer.close();
     	} catch (IOException e) {
     		System.out.println("Something went wrong in writing the ttl file.");
@@ -136,26 +135,79 @@ public class ModelRDF {
         return builder;
     }
 	
-    public static ModelBuilder extractRelation(MegaModel mm, ModelBuilder rdfModel) {
+    // getting RelationshipInstanceMap x : z y
+    public static void extractRelationshipInstance(MegaModel mm, ModelBuilder rdfModel) {
         // Getting Subject - Predicate - Object
         for (Entry<String, Set<Relation>> entry : 
             mm.getRelationshipInstanceMap().entrySet()) {
             // get String keys
-            String key = entry.getKey();
+            String predicate = entry.getKey();
             // get Relation values
             Set<Relation> megaRl = entry.getValue();
             for(Relation i: megaRl) {
-                _addRelations(rdfModel, i.getSubject(), key, i.getObject());
+            	String subject = i.getSubject();
+            	String object = i.getObject();
+                _addRelations(rdfModel, subject, predicate, object);
             }
         }
-        System.out.println("done adding relations");
-        return rdfModel;
+        System.out.println("done adding relationship instances");
+//        return rdfModel;
     }
 	
+    // getting instanceOfMap x : y
+    public static void extractInstanceOf(MegaModel mm, ModelBuilder rdfModel) {
+    	for (Entry<String, String> entry :
+    		mm.getInstanceOfMap().entrySet()) {
+    		String subject = entry.getKey();
+    		String object = entry.getValue();
+    		_addRelations(rdfModel, subject, "instanceOf", object);
+    		
+    	}
+    	System.out.println("done adding Instances of relationships");
+    }
+    
+    // getting RelationshipDeclarationMap z < x # y
+    public static void extractRelationshipDeclaration(MegaModel mm, ModelBuilder rdfModel) {   	
+    	for (Entry<String, Set<Relation>> entry:
+    		mm.getRelationshipDeclarationMap().entrySet()) {
+    		String predicate = entry.getKey();
+    		Set<Relation> megaRl = entry.getValue();
+    		for (Relation i: megaRl) {
+    			String subject = i.getSubject();
+    			String object = i.getObject();
+    			_addRelations(rdfModel, subject, predicate, object);
+    		}
+    	}
+    	System.out.println("done adding relationship declarations");
+    }
+    
+    // getting LinkMap z = ""
+    public static void extractLinkMap(MegaModel mm, ModelBuilder rdfModel) {
+    	for (Entry<String, Set<String>> entry:
+    		mm.getLinkMap().entrySet()) {
+    		String subject = entry.getKey();
+    		Set<String> link = entry.getValue();
+    		for (String i: link) {
+    			String object = i;
+    			_addRelations(rdfModel, subject, "Link", object);
+    		}
+    	}
+    	System.out.println("done adding links");
+    }
+    
+    // geting Subtypes x < y
+    public static void extractSubtypes(MegaModel mm, ModelBuilder rdfModel) {
+    	for (Entry<String, String> entry:
+    		mm.getSubtypesMap().entrySet()) {
+    		String subject = entry.getKey();
+    		String object = entry.getValue();
+    		_addRelations(rdfModel, subject, "SubTypeOf", object);
+    	}
+    	System.out.println("done adding subtypes");    	
+    }
+
+    // adding statements
     public static void _addRelations(ModelBuilder rdfModel, String subject, String predicate, String object) {
-        
-        // TODO make them proper IRIs
-        // should they be IRI 
 //    	ValueFactory vf = SimpleValueFactory.getInstance();
 //    	IRI sub = vf.createIRI(prefix, subject);
 //        Literal obj = vf.createLiteral(object);
@@ -164,9 +216,11 @@ public class ModelRDF {
 //        subject = prefix + subject;
 //        object = prefix + object;
 //        predicate = prefix + predicate;
-    	rdfModel.subject("XML:" + subject).add("XML:" + predicate, object);
 //    	rdfModel.add(sub, pre, obj);
+    	rdfModel.subject("XML:" + subject).add("XML:" + predicate, object);
     }
+    // getfunctiondeclaration / application look into rdf lists and sets
+    // insert SPARQL statements
     
 }
 
